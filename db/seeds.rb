560e169c2f1684e5c930f617ad8cef9ee0c646bd
Ollie <oliverbucks@gmail.com>
2017-09-04 18:51:20 +0000

    added posts & seeded 100

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+
+
+100.times do |post|
+  Post.create!(date: Date.today, rationale:"This is the rationale for post number #{post}")
+end
+
+puts "100 posts have been created"
+
+
+
+
+
