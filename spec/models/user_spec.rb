da2758843bf32cfa379a085e55a3a81976682d7b
Ollie <oliverbucks@gmail.com>
2017-09-05 12:57:27 +0000

    added edit functionality & rspec working

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "creation" do
-  	before do
-  		@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-  	end
+  before do
+    @user = FactoryGirl.create(:user)
+  end
 
-  	it "User can be created" do
+  describe "creation" do
+  	it "can be created" do
   		expect(@user).to be_valid
   	end
 
-  	it "User cannot be created without first_name, last_name" do
+  	it "cannot be created without first_name, last_name" do
   		@user.first_name = nil
   		@user.last_name = nil
   		expect(@user).to_not be_valid
   	end
-  	describe "custom full name method" do
-  	  it "combines first and last name" do
-  	    expect(@user.full_name).to eq("SNOW, JON")
-  	  end
-  	end
+  end
+
+  describe "custom name methods" do
+    it 'has a full name method that combines first and last name' do
+      expect(@user.full_name).to eq("SNOW, JON")
+    end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,7 @@ require 'capybara/rails'
 include Warden::Test::Helpers
 Warden.test_mode!
 
+
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
@@ -21,4 +22,6 @@ RSpec.configure do |config|
   config.after(:each) { DatabaseCleaner.clean }
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.include FactoryGirl::Syntax::Methods
 end
+
