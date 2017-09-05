a9d701fdece23a7aaba09601905a45803af288e5
Ollie <oliverbucks@gmail.com>
2017-09-05 10:46:14 +0000

    add full_name def and @posts partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-validates_presence_of :first_name, :last_name
- has_many :posts
+  validates_presence_of :first_name, :last_name
+  has_many :posts
+ 
+  def full_name
+    last_name.upcase + ", " + first_name.upcase
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,14 +6,19 @@ RSpec.describe User, type: :model do
   		@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
   	end
 
-  	it "can be created" do
+  	it "User can be created" do
   		expect(@user).to be_valid
   	end
 
-  	it "cannot be created without first_name, last_name" do
+  	it "User cannot be created without first_name, last_name" do
   		@user.first_name = nil
   		@user.last_name = nil
   		expect(@user).to_not be_valid
+  	end
+  	describe "custom full name method" do
+  	  it "combines first and last name" do
+  	    expect(@user.full_name).to eq("SNOW, JON")
+  	  end
   	end
   end
 end
