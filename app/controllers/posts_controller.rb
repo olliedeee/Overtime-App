b366dddcc1baa76e14df7c362797382cb86f1004
Ollie <oliverbucks@gmail.com>
2017-09-06 13:46:34 +0000

    added admin-only to edit for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,10 +21,12 @@ class PostsController < ApplicationController
 	end
 
 	def edit
+		authorize @post
 	end
 	
 	def update
-		if @post.update(post_params)
+		authorize @post
+	if @post.update(post_params)
 			redirect_to @post, notice: 'Your post was edited successfully'
 		else
 			render :edit
