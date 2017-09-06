b6f663d48af3fa406dc3d0c26a74ed811c27d07f
Ollie <oliverbucks@gmail.com>
2017-09-06 14:46:43 +0000

    added redirect to non-auth access and rspec tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
 	include Pundit
 	protect_from_forgery with: :exception
   before_action :authenticate_user!
+  
+  
+   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    redirect_to root_path, notice: "You are not authorized to perform this action."
+  end
 end
