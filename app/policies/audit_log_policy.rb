4ccebc628e624167b30bea1562bc7180a8584d72
Ollie <oliverbucks@gmail.com>
2017-09-08 14:26:24 +0000

    add audit log tab for admin view only

---
+++ app/policies/audit_log_policy.rb
@@ -0,0 +1,15 @@
+class AuditLogPolicy < ApplicationPolicy
+
+  def index?
+    return true if  admin?
+  end
+  
+  
+  private
+  
+
+  def admin?
+    admin_types.include?(user.type)
+  end
+
+end
